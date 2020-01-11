471055e926e5739ed093f29dc82e02dc85d3dc94
Bodhish Thomas <bodhish@gmail.com>
2020-01-12 01:20:05 +0530

    Add active admin

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'devise', '~> 4.7', '>= 4.7.1' # User auth library.
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'activeadmin', '~> 2.6.0' # The administration framework for Ruby on Rails applications. https://activeadmin.info
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ lib/active_admin/current_admin_user.rb
@@ -0,0 +1,21 @@
+module ActiveAdmin
+  module CurrentAdminUser
+    extend ActiveSupport::Concern
+
+    included do
+      helper_method :current_admin_user
+    end
+
+    def current_admin_user
+      @current_admin_user ||= AdminUser.where(email: current_user&.email).first
+    end
+
+    def authenticate_admin_user!
+      authenticate_user!
+      return if current_admin_user.present?
+
+      flash[:notice] = 'You are not an administrator!'
+      redirect_to root_url
+    end
+  end
+end
